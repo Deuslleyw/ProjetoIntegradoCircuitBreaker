094c6eb273ddbb36d6a7e87dc13dc1851ca04d50
Deuslleyw <Deuslleyw@hotmail.com>
2024-07-08 23:40:11 -0300

    firstCommit

---
+++ src/main/java/com/deusleyDev/api_publication/ApiPublicationApplication.java
@@ -0,0 +1,15 @@
+package com.deusleyDev.api_publication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPublicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPublicationApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/deusleyDev/api_publication/ApiPublicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusleyDev.api_publication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPublicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
