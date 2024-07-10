45c762c7a030f0d3b637da46b2c3e300d0ef2943
Deuslleyw <Deuslleyw@hotmail.com>
2024-07-10 14:06:09 -0300

    implDomains

---
+++ src/main/java/com/deusleyDev/api_publication/domain/Comment.java
@@ -0,0 +1,14 @@
+package com.deusleyDev.api_publication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    private String author;
+    private String text;
+}
