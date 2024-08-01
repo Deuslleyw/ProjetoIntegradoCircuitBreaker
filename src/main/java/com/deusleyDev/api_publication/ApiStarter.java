6df41cfe808b0d308066f8679d94bb1602c20fb0
Deuslleyw <Deuslleyw@hotmail.com>
2024-07-31 22:47:13 -0300

    update

--- src/main/java/com/deusleyDev/api_publication/ApiStarter.java
+++ src/main/java/com/deusleyDev/api_publication/ApiStarter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiPublicationApplication {
+public class ApiStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiPublicationApplication.class, args);
+		SpringApplication.run(ApiStarter.class, args);
 
 
 	}
---
+++ src/main/java/com/deusleyDev/api_publication/exceptions/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.deusleyDev.api_publication.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+
+    private Long timestamp;
+    private Integer status;
+    private String message;
+    private String path;
+
+}
+
+
+
