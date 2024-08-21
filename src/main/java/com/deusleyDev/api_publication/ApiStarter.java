a5977279ec3cb82c098d8ee51955c6a24de6df76
Deuslleyw <Deuslleyw@hotmail.com>
2024-08-21 17:21:43 -0300

    fixbugCloud

--- src/main/java/com/deusleyDev/api_publication/ApiStarter.java
+++ src/main/java/com/deusleyDev/api_publication/ApiStarter.java
@@ -1,15 +1,18 @@
 package com.deusleyDev.api_publication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAutoConfiguration
 public class ApiStarter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiStarter.class, args);
+
 
 
 	}
